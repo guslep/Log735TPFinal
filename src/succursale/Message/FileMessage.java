88261df2d3bb1d2b186ad185c10affbbd833979c
unknown <AJ86170@logti-a3322-06.ens.ad.etsmtl.ca>
2015-07-09 11:05:16 -0400

    fix

---
+++ src/succursale/Message/FileMessage.java
@@ -0,0 +1,32 @@
+package succursale.Message;
+
+import java.io.Serializable;
+/***
+ * message d'envoi entre les serveurs pour la copie des fichiers. permet de savoir la taille du fichier, contient des bytes du fichier, ainsi que la position d'écriture.
+ * @author Marc
+ *
+ */
+public class FileMessage extends Message implements Serializable{
+	private byte[] byteArray;
+	private String filename;
+	private int position;
+	
+	
+	public FileMessage(byte[] byteArray, String filename, int position){
+		this.byteArray = byteArray;
+		this.filename = filename;
+		this.position = position;
+	}
+	
+	
+	public String getFileName(){
+		return filename;
+	}
+	public byte[] getByteArray(){
+		return byteArray;
+	}
+	public int getPosition() {
+		return position;
+	}
+	
+}
